9b0bf162cbf96bc79f668bc26dddd1eb8fa08223
Wavinyaa <mitchellewavinya@gmail.com>
2023-01-24 14:42:00 -0800

    Task 0

--- deck.h
+++ deck.h
@@ -1,12 +1,14 @@
 #ifndef DECK_H
 #define DECK_H
-/*Structs*/
+
+#include <stdlib.h>
+
 /**
- * enum kind_e - Card suit
- * @SPADE: spade suit
- * @HEART: heart suit
- * @CLUB: club suit
- * @DIAMOND: diamond suit
+ * enum kind_e - Enumeration of card suits.
+ * @SPADE: Spades suit.
+ * @HEART: Hearts suit.
+ * @CLUB: Clubs suit.
+ * @DIAMOND: Diamonds suit.
  */
 typedef enum kind_e
 {
@@ -42,7 +44,7 @@ typedef struct deck_node_s
 	struct deck_node_s *prev;
 	struct deck_node_s *next;
 } deck_node_t;
-/*Prototypes*/
-void sort_deck(deck_node_t **deck);
-#endif
 
+void sort_deck(deck_node_t **deck);
+
+#endif /* DECK_H */
